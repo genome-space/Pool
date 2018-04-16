00d8d1b4add17b8cee71eb97b942157623c3c142
François-David Collin <fradav@gmail.com>
2018-04-16 15:40:46 +0200

    WIp F-bound

--- Pool.hpp
+++ Pool.hpp
@@ -1,85 +1,81 @@
 #pragma once
 
-#include <memory>
 #include <vector>
-#include <functional>
 #include <map>
-#include <type_traits>
-#include <iostream>
 
-template <class T, std::size_t ChunkSize = 1>
-struct Pool
+template <class T, std::size_t ChunkSize>
+class Pool
 {
-    static_assert(ChunkSize > 0);
 
-    std::map<std::size_t,std::size_t> h2i;
+  private:
     std::vector<std::size_t> free_objects;
-    std::vector<std::unique_ptr<T>> indexed_objects;
-    std::vector<std::size_t> allocated_ids;
+    std::map<std::size_t, std::size_t> h2i;
+    using del_ptr = typename UniqueProxy<T>::del_ptr;
+    std::vector<del_ptr> indexed_objects;
 
-    void del_object(T *ptr_obj) {
-        auto h = std::hash<T*>()(ptr_obj);
-        auto i = h2i[h];
-        std::cout << "Deleting object : {" << h << "," << i << "}" << std::endl;
-        free_objects.push_back(i);
-        indexed_objects[i] = std::unique_ptr<T>{ptr_obj};
+    auto &get_object()
+    {
+        auto new_obj = T();
+        auto &unique_obj = del_ptr{&new_obj, _del_fun};
+        return std::move(unique_obj);
     }
 
-    using del_type = std::function<void(T *)>;
-    del_type del_fun = std::bind(&Pool::del_object,this,std::placeholders::_1);;
-    using del_ptr = std::unique_ptr<T, del_type>;
+    void del_object(std::size_t h, T *ptr_obj)
+    {
+        auto i = h2i[h];
+        free_objects.push_back(i);
+        indexed_objects[i] = del_ptr{ptr_obj};
+    }
 
-    using hash_t = std::hash<typename Pool<T, ChunkSize>::del_ptr>;
-    static std::size_t hash(del_ptr& t) { return hash_t()(t); }
-    std::map<std::size_t,del_ptr> allocated_objects;
-
-  public:
-
-    auto& get_object()
+    del_ptr &pool_allocate()
     {
         if (free_objects.empty())
         {
             free_objects.resize(ChunkSize);
             auto oldsize = indexed_objects.size();
-            indexed_objects.resize(oldsize+ChunkSize);
-            for(auto i = 0; i < ChunkSize; i++) {
+            indexed_objects.resize(oldsize + ChunkSize);
+
+            for (auto i = 0; i < ChunkSize; i++)
+            {
                 free_objects[oldsize + i] = oldsize + ChunkSize - i - 1;
-                auto new_obj = std::make_unique<T>();
-                h2i[std::hash<std::unique_ptr<T>>()(new_obj)] = oldsize + i;
-                indexed_objects[oldsize + i] = std::move(new_obj);
+                auto &new_obj = _unique_proxy.get_object();
+                h2i[UniqueProxy<T>::hash(new_obj)] = oldsize + i;
             }
-        } 
+        }
         auto i = free_objects.back();
-//        std::cout << "getting object n : " << i << std::endl;        
         free_objects.pop_back();
-        std::unique_ptr<T> available_object = std::move(indexed_objects[i]);
-        allocated_objects.emplace(i,del_ptr{available_object.release(),del_fun});
-        return allocated_objects[i];
+        return std::move(indexed_objects[i]);
     }
 
-    void display_free() {
+  public:
+
+    void display_free()
+    {
         std::cout << "free_objects : ";
-        for(auto i : free_objects) std::cout << i << " ";
+        for (auto i : free_objects)
+            std::cout << i << " ";
         std::cout << std::endl;
         std::cout << "hash objects : ";
-        for(auto p : h2i) std::cout << " {" << p.first << "," << p.second << "}";
+        for (auto p : h2i)
+            std::cout << " {" << p.first << "," << p.second << "}";
         std::cout << std::endl;
     }
 
-    std::size_t size() {
+    std::size_t size()
+    {
         return indexed_objects.size() - free_objects.size();
     }
 
-    std::size_t capacity() {
+    std::size_t capacity()
+    {
         return indexed_objects.size();
     }
 
-    std::size_t get_i(del_ptr& t) { return h2i[hash(t)]; }
-    
-    void erase(del_ptr& t) { allocated_objects.erase(get_i(t)); t.reset(nullptr); }
+    std::size_t get_i(del_ptr &t) { return h2i[UniqueProxy<T>::hash(t)]; }
 
-    void obj_info(del_ptr& t) {
-        std::cout << "value : " << *t << ", i : " << get_i(t) <<  ", ii : " << ", hash : " << hash(t) << std::endl;
+    void obj_info(del_ptr &t)
+    {
+        std::cout << "value : " << *t << ", i : " << get_i(t) << ", ii : "
+                  << ", hash : " << hash(t) << std::endl;
     }
-
 };
--- main.cpp
+++ main.cpp
@@ -8,39 +8,39 @@ using namespace std;
 
 int main()
 {
-    Pool<int, 10> pool;
-    cout << "Entering bloc1" << endl;
-    {
-        cout << "Getting our first object" << endl;
-        auto& i1 = pool.get_object();
-        cout << "Pool size : " << pool.size() << " capacity : " << pool.capacity() << endl;
-        *i1 = 2;
-        pool.obj_info(i1);
-        pool.display_free();
-        cout << "Getting our second object" << endl;
-        auto& i2 = pool.get_object();
-        pool.display_free();
-        cout << "Pool size : " << pool.size() << " capacity : " << pool.capacity() << endl;
-        *i2 = 3;
-        pool.obj_info(i2);
-        cout << "Exiting from bloc1" << endl;
-        pool.erase(i1);
-        pool.erase(i2);
-    }
-    cout << "Pool size : " << pool.size() << " capacity : " << pool.capacity() << endl;
-        cout << "Getting our first object" << endl;
-        auto& i1 = pool.get_object();
-        cout << "Pool size : " << pool.size() << " capacity : " << pool.capacity() << endl;
-        *i1 = 2;
-        pool.obj_info(i1);
-        pool.display_free();
-        cout << "Getting our second object" << endl;
-        auto& i2 = pool.get_object();
-        pool.display_free();
-        cout << "Pool size : " << pool.size() << " capacity : " << pool.capacity() << endl;
-        *i2 = 3;
-        pool.obj_info(i2);
-        cout << "Getting second object by index " << endl;
-        cout << "Exiting..." << endl;
-        cout.flush();
+    Pool<int> pool(12);
+    // cout << "Entering bloc1" << endl;
+    // {
+    //     cout << "Getting our first object" << endl;
+    //     auto& i1 = pool.allocate();
+    //     cout << "Pool size : " << pool.size() << " capacity : " << pool.capacity() << endl;
+    //     *i1 = 2;
+    //     pool.obj_info(i1);
+    //     pool.display_free();
+    //     cout << "Getting our second object" << endl;
+    //     auto& i2 = pool.allocate();
+    //     pool.display_free();
+    //     cout << "Pool size : " << pool.size() << " capacity : " << pool.capacity() << endl;
+    //     *i2 = 3;
+    //     pool.obj_info(i2);
+    //     cout << "Exiting from bloc1" << endl;
+    //     // pool.erase(i1);
+    //     // pool.erase(i2);
+    // }
+    // cout << "Pool size : " << pool.size() << " capacity : " << pool.capacity() << endl;
+    //     cout << "Getting our first object" << endl;
+    //     auto& i1 = pool.get_object();
+    //     cout << "Pool size : " << pool.size() << " capacity : " << pool.capacity() << endl;
+    //     *i1 = 2;
+    //     pool.obj_info(i1);
+    //     pool.display_free();
+    //     cout << "Getting our second object" << endl;
+    //     auto& i2 = pool.get_object();
+    //     pool.display_free();
+    //     cout << "Pool size : " << pool.size() << " capacity : " << pool.capacity() << endl;
+    //     *i2 = 3;
+    //     pool.obj_info(i2);
+    //     cout << "Getting second object by index " << endl;
+    //     cout << "Exiting..." << endl;
+    //     cout.flush();
 }
